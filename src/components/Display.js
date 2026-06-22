1b52c11c5e408a226df51be81911abd598bca671
aisatmariano224488 <aisat.mariano224488@gmail.com>
2026-06-22 18:02:51 +0800

    feat: enhance UI layout and button styles for better user experience

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,7 @@
 const Display = (value) => {
     return `
-        <section class="px-4 bg-neutral">
-            <div class="p-4 text-white text-end text-4xl">${value}</div>
+        <section class="">
+            <div class="px-4 pb-4 pt-8 text-white text-end text-4xl">${value}</div>
         </section>
     `;
 }
